e4207756b33588c6e2aa74681554a0cb77f40d83
Pinki Paul <pp3785828@gmail.com>
2026-05-20 13:15:48 +0530

    version 2

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || ''
 export default function App() {
   const tree = (
     <AuthProvider>
-      <BrowserRouter basename="/agentforcex">
+      <BrowserRouter basename="/agentforcex/v2">
         <AppRoutes />
         <Chatbot />
       </BrowserRouter>
